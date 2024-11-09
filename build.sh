d729e1ceb60f8ea0cafe5da1bac45681170144d8
BruhDudeWew <Me45672@gmail.com>
2024-11-09 08:23:37 +0700

    Rework build script a bit

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-export PATH=../prebuilts/gas/linux-x86:$PATH
-export PATH=../prebuilts-master/clang/host/linux-x86/clang-r416183b/bin:$PATH
-export PATH=../prebuilts/build-tools/path/linux-x86:$PATH
+#!/bin/bash
+
+echo "The script will assume that the toolchains is in the .. folder"
+echo "Might make the script check for toolchains existance and boot image create later"
+
+export PATH=$(realpath ../prebuilts/gas/linux-x86):$(realpath ../prebuilts-master/clang/host/linux-x86/clang-r416183b/bin):$(realpath ../prebuilts/build-tools/path/linux-x86):$PATH
 
 export LLVM=1
 export LLVM_IAS=1
@@ -12,5 +15,5 @@ export CROSS_COMPILE=aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=s
 export PLATFORM_VERSION=12
 
-make -j12 O=out bruh-waipio-gki_defconfig
-make -j12 O=out Image
+make -j$(nproc --all) O=out bruh-waipio-gki_defconfig
+make -j$(nproc --all) O=out Image
